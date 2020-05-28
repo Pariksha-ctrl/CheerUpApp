44e2a3807ce214a57c926c5bf05dc758fc9440e6
ParikshaGit <smartgirl@gmail.com>
2020-05-28 23:05:36 +1000

    Worked on database insert, update, delete, get all and some other methods

--- app/src/main/java/com/example/cheerupapp/database/SongDatabaseHelper.java
+++ app/src/main/java/com/example/cheerupapp/database/SongDatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.cheerupapp.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,10 +10,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 // database helper class
 public class SongDatabaseHelper extends SQLiteOpenHelper {
 
-    // this is the instance which all the databases will share
+    // this is the instance which all objects of this databases will share
     private static SongDatabaseHelper songInstance = null;
     private Context context;
 
@@ -33,26 +38,28 @@ public class SongDatabaseHelper extends SQLiteOpenHelper {
             COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             COL_NAME + " TEXT, " +
             COL_FAVORITE_VERSE + " TEXT, " +
-            COL_SINGER + " TEXT, " +
+            COL_SINGER + " TEXT DEFAULT 'Sonu Nigum'," +
             COL_IMAGE + " TEXT, " +
-            COL_RATING + " INTEGER DEFAULT 0)";
+            COL_RATING + " INTEGER )";
 
-    private static final String DROP_TABLE_;
+    // if table doesn't exists it will give an error and delete that table only if it exists in database
+    private static final String DROP_TABLE_STRING = "DROP_TABLE_IF_EXISTS, " + TABLE_NAME;
+    // this will return all the data from the database
+    private static final String GET_ALL_STRING = "SELECT * FROM " + TABLE_NAME;
 
     public SongDatabaseHelper(Context applicationContext) {
         super();
     }
 
-
     // this method is use to avoiding creating large number of instance of Song
-public static synchronized SongDatabaseHelper getInstance(Context context){
-    if (songInstance == null){
-        //this is executed only for the first time
-        songInstance = new SongDatabaseHelper(context.getApplicationContext());
+    public static synchronized SongDatabaseHelper getInstance(Context context){
+        if (songInstance == null){
+            //this is executed only for the first time
+            songInstance = new SongDatabaseHelper(context.getApplicationContext());
+        }
+        //when Song is not null anymore
+        return songInstance;
     }
-    //when Song is not null anymore
-    return songInstance;
-}
 
 
     public SongDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
@@ -67,13 +74,87 @@ public static synchronized SongDatabaseHelper getInstance(Context context){
         super(context, name, version, openParams);
     }
 
+    // this method is executed only if the database does not exists
     @Override
     public void onCreate(SQLiteDatabase sqLitedb) {
         sqLitedb.execSQL(CREATE_TABLE_STRING);
     }
 
+    // this method will executed every time we chase the database version
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase sqLitedb, int oldVersion, int newVersion) {
+        sqLitedb.execSQL(DROP_TABLE_STRING);
+        onCreate(sqLitedb);
+    }
 
+    // INSERT METHOD
+    public Long insert(String name, String favoriteVerse, Long rating){
+        // creating an instance of SQLITE database
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_NAME, name);
+        contentValues.put(COL_FAVORITE_VERSE, favoriteVerse);
+        contentValues.put(COL_RATING, rating);
+        contentValues.put(COL_IMAGE, getRandomImageName());
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        db.close();
+        // insert will not be performed due to error, if result is -1
+        return result;
+    }
+
+    // UPDATE METHOD
+    public boolean update(Long id, String name, String favoriteVerse, Long rating){
+        // creating an instance of SQLITE database
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_ID, id);
+        contentValues.put(COL_NAME, name);
+        contentValues.put(COL_FAVORITE_VERSE, favoriteVerse);
+        contentValues.put(COL_RATING, rating);
+
+        int updatedNumOfRows = db.update(TABLE_NAME, contentValues, "ID=?", new String[]{id.toString()});
+        db.close();
+        return updatedNumOfRows == 1;
+    }
+
+    public boolean delete(Long id){
+        // creating an instance of SQLITE database
+        SQLiteDatabase db = this.getWritableDatabase();
+        int deletedNumOfRows = db.delete(TABLE_NAME, "ID=?", new String[]{id.toString()});
+        db.close();
+        return deletedNumOfRows == 1;
+    }
+
+    // using random function
+    private String getRandomImageName(){
+        Random randomImage = new Random();
+        int imageNum = randomImage.nextInt(10) + 1;
+        return  "Song _" + imageNum;
+    }
+
+    // returning the list all songs from the SONG database table
+    public List<Song> getSongs(){
+        List<Song> songs = new ArrayList<>();
+        Cursor cursor = getAll();
+
+        if (cursor.getCount() > 0){
+            Song song;
+            while (cursor.moveToNext()){
+                Long id = cursor.getLong(0);
+                String name = cursor.getString(1);
+                String favoriteVerse = cursor.getString(2);
+                String singer = cursor.getString(3);
+                String imageName = cursor.getString(4);
+                Long rating = cursor.getLong(5);
+
+                song = new Song(id, name, favoriteVerse, singer, imageName, rating);
+                songs.add(song);
+            }
+        }
+        cursor.close();
+        return songs;
     }
 }
